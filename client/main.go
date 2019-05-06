e4c9c86fb2bd5caf975abb4266a571923189a6ee
dmittelstaedt <mittelstaedt.david@gmail.com>
2019-05-06 21:00:17 +0200

    Add simple client for retrieving user and users

--- client/main.go
+++ client/main.go
@@ -1,60 +1,96 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"text/tabwriter"
+)
 
+// User represents a user.
+type User struct {
+	ID        int    `json:"id"`
+	FirstName string `json:"firsname"`
+	LastName  string `json:"lastname"`
+	Name      string `json:"name"`
+	Realm     string `json:"realm"`
+	Role      string `json:"role"`
+	Password  string `json:"password"`
 }
 
-// import (
-// 	"encoding/json"
-// 	"net/http"
-// 	"testing"
-// )
+const userHeader = "ID\tFIRSTNAME\tLASTNAME\tNAME\tROLE"
+
+func printUser(user User) {
+	w := new(tabwriter.Writer)
+	w.Init(os.Stdout, 0, 8, 0, '\t', 0)
+	fmt.Fprintln(w, userHeader)
+	fmt.Fprintln(w, strconv.Itoa(user.ID)+"\t"+user.FirstName+"\t"+user.LastName+"\t"+user.Name+"\t"+user.Role)
+	fmt.Fprint(w)
+	w.Flush()
+}
+
+func printUsers(users []User) {
+	w := new(tabwriter.Writer)
+	w.Init(os.Stdout, 0, 8, 0, '\t', 0)
+	fmt.Fprintln(w, userHeader)
+	for _, user := range users {
+		fmt.Fprintln(w, strconv.Itoa(user.ID)+"\t"+user.FirstName+"\t"+user.LastName+"\t"+user.Name+"\t"+user.Role)
+	}
+	fmt.Fprint(w)
+	w.Flush()
+}
+
+func executeRequest(method, url string) *http.Response {
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println(err)
+	}
+
+	return resp
+}
+
+func getUsers() []User {
+	resp := executeRequest("GET", "http://localhost:8080/api/v1/users")
+	defer resp.Body.Close()
+
+	var users []User
+	if err := json.NewDecoder(resp.Body).Decode(&users); err != nil {
+		log.Println(err)
+	}
+
+	return users
+}
+
+func getUser(id int) User {
+	resp := executeRequest("GET", "http://localhost:8080/api/v1/users/"+strconv.Itoa(id))
+	defer resp.Body.Close()
+
+	var user User
+	if err := json.NewDecoder(resp.Body).Decode(&user); err != nil {
+		log.Println(err)
+	}
+
+	return user
+}
+
+func main() {
+	users := getUsers()
+	printUsers(users)
+
+	user := getUser(1)
+	printUser(user)
+}
 
 // Create substrings --> Move to Client logic
 // substrs := strings.Split(params["permission-name"], "-")
 // dir := strings.Join(substrs[:len(substrs)-1], "-")
 // rw := strings.Join(substrs[len(substrs)-1:], "")
-
-// func TestReadUsers(t *testing.T) {
-// 	client := &http.Client{}
-// 	req, err := http.NewRequest("GET", "http://localhost:8080/users", nil)
-// 	if err != nil {
-// 		t.Log(err)
-// 	}
-// 	resp, err := client.Do(req)
-// 	if err != nil {
-// 		t.Log(err)
-// 	}
-// 	defer resp.Body.Close()
-
-// 	statusCode := resp.StatusCode
-// 	if statusCode != 200 {
-// 		t.Errorf("readUsers was incorrect, got %v, want: %v", statusCode, 200)
-// 	}
-
-// 	var users []User
-// 	if err := json.NewDecoder(resp.Body).Decode(&users); err != nil {
-// 		t.Log("Error during parsing respone body")
-// 	}
-// 	if len(users) != 7 {
-// 		t.Errorf("readUsers was incorrect, got %v, want %v", len(users), 7)
-// 	}
-
-// 	dummy := User{
-// 		ID:        1,
-// 		FirstName: "David",
-// 		LastName:  "Mittelstaedt",
-// 		Name:      "david",
-// 		Role:      "app_admin",
-// 	}
-
-// 	found := false
-// 	for _, user := range users {
-// 		if user == dummy {
-// 			found = true
-// 		}
-// 	}
-// 	if !found {
-// 		t.Errorf("readUsers was incorrect, got %v, want %v", "", dummy)
-// 	}
-// }
