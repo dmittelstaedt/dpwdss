15ad02671696e4d15de94c6812c2723398104fcc
dmittelstaedt <mittelstaedt.david@gmail.com>
2019-05-12 19:53:54 +0200

    Add function for reading and printing permissions of specific user

--- client/main.go
+++ client/main.go
@@ -1,8 +1,30 @@
 package main
 
-import "fmt"
-
 const apiEndpoint = "http://localhost:8080/api/v1/"
+
+func getUserPermissions() {
+	user := readUser(4)
+	permissions := readPermissions()
+
+	var userPermissions []Permission
+
+	for _, permission := range permissions {
+		if permission.UserID == user.ID {
+			userPermissions = append(userPermissions, permission)
+		}
+	}
+
+	var permissionsOut []PermissionOut
+	for _, permission := range userPermissions {
+		group := readGroup(permission.GroupID)
+		permissionsOut = append(permissionsOut, PermissionOut{
+			ID:        permission.ID,
+			UserName:  user.Name,
+			GroupName: group.Name,
+		})
+	}
+	printPermissions(permissionsOut)
+}
 
 func main() {
 	// users := readUsers()
@@ -15,14 +37,16 @@ func main() {
 
 	// fmt.Println("")
 
-	groups := readGroups()
-	printGroups(groups)
+	// groups := readGroups()
+	// printGroups(groups)
 
-	fmt.Println("")
-	group := readGroup(1)
-	printGroup(group)
+	// fmt.Println("")
+	// group := readGroup(1)
+	// printGroup(group)
 
-	fmt.Println("")
+	// fmt.Println("")
+
+	getUserPermissions()
 }
 
 // Create substrings --> Move to Client logic
